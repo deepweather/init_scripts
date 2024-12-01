ff9dd717501f489ba3a5fc24f131fcc36d9c12af
marvingabler <marvin@jua.ai>
2024-12-01 18:28:42 +0000

    aded user create script

---
+++ create_sudo_user_same_keys.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo adduser $1 --disabled-password 
+sudo usermod -aG sudo $1
+sudo mkdir -p /home/$1/.ssh
+sudo cp /root/.ssh/authorized_keys /home/$1/.ssh/
+sudo chown -R $1:$1 /home/$1/.ssh
+sudo chmod 700 /home/$1/.ssh
+sudo chmod 600 /home/$1/.ssh/authorized_keys
